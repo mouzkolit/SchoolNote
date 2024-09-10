7675420ca2bde64daec64ea1ca0727a630087411
Maximilian Zeidler <maximilian.zeidler@omiqa.bio>
2024-09-10 14:56:16 +0200

    added jwt token authentication workflow for access of the applicaiton

--- api/routing/course.go
+++ api/routing/course.go
@@ -8,18 +8,20 @@ import (
 )
 
 func CreateCourse(r *gin.Engine, db *database.DB) {
-	r.POST("/:class/course", func(c *gin.Context) {
+	r.POST("/school/:id/:class/course", func(c *gin.Context) {
 		className := c.Param("class")
+		schoolID := c.Param("id")
 		courseName := c.Query("course")
 		courseType := c.Query("Type")
 		println("this is the course type: %s", courseType)
 		println("this is the course name: %s", courseName)
 		println("this is the class name %s", className)
+		println("this is the school id %s", schoolID)
 	})
 }
 
 func GetCourse(r *gin.Engine, db *database.DB) {
-	r.GET("/:class/course/:courseid", func(c *gin.Context) {
+	r.GET("/school/:id/:class/course/:courseid", func(c *gin.Context) {
 		courseID, err := strconv.ParseInt(c.Param("courseid"), 10, 64)
 		if err != nil {
 			println("error here %s", err)
